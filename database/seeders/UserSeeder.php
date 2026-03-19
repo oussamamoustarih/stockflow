72f4fece7f6e27aa4f4a3b92d2d38a1baf40f177
oussamamoustarih <oussamamoustarih@gmail.com>
2026-03-19 15:23:38 +0000

    chore(db): update database port and user passwords
    - Change default DB_PORT from 3306 to 3307 in .env.example and config/database.php
    - Update seeded user passwords to 'StockFlow@2026' for security in UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,28 +13,28 @@ class UserSeeder extends Seeder
         User::create([
             'name' => 'Administrateur',
             'email' => 'admin@stock.ma',
-            'password' => Hash::make('password'),
+            'password' => Hash::make('StockFlow@2026'),
             'role' => 'admin',
         ]);
 
         User::create([
             'name' => 'Gestionnaire Stock',
             'email' => 'gestionnaire@stock.ma',
-            'password' => Hash::make('password'),
+            'password' => Hash::make('StockFlow@2026'),
             'role' => 'gestionnaire',
         ]);
 
         User::create([
             'name' => 'Vendeur Caisse',
             'email' => 'vendeur@stock.ma',
-            'password' => Hash::make('password'),
+            'password' => Hash::make('StockFlow@2026'),
             'role' => 'vendeur',
         ]);
 
         User::create([
             'name' => 'Manager Gérant',
             'email' => 'manager@stock.ma',
-            'password' => Hash::make('password'),
+            'password' => Hash::make('StockFlow@2026'),
             'role' => 'manager',
         ]);
     }
